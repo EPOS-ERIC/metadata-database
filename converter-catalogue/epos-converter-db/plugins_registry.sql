b4849fba5ba802ed5d147c4ae89b11bf21a49676
Marco Salvi <marco.salvi@ingv.it>
2025-04-02 15:06:00 +0200

    converter catalogue refactor & fixes

--- converter-catalogue/epos-converter-db/plugins_registry.sql
+++ converter-catalogue/epos-converter-db/plugins_registry.sql
@@ -1,25 +1,30 @@
+\connect converter_catalogue;
 BEGIN;
 
+CREATE TYPE version_type_enum AS ENUM ('branch', 'tag');
+
 CREATE TABLE plugin (
-	id character varying(1024) NOT NULL PRIMARY KEY,
-	software_source_code_id VARCHAR(1024) NOT NULL,
-	software_application_id VARCHAR(1024) NOT NULL,
-	version VARCHAR(1024) NOT NULL,
-	proxy_type VARCHAR(1024) NOT NULL,
-	runtime VARCHAR(1024) NOT NULL,
-	execution VARCHAR(1024) NOT NULL,
-	installed BOOLEAN NOT NULL DEFAULT FALSE,
-	enabled BOOLEAN NOT NULL DEFAULT FALSE
+	id character varying(1024) NOT NULL PRIMARY KEY,	-- the id of the plugin (generated when the plugin is created)
+	name VARCHAR(1024) NOT NULL,						-- the name of the plugin
+	description VARCHAR(1024) NOT NULL,					-- a description of the plugin
+	version VARCHAR(1024) NOT NULL,						-- the name of the branch if version_type is branch or the tag number if it is tag
+	version_type version_type_enum NOT NULL,				-- either 'branch' or 'tag' 
+	repository VARCHAR(1024) NOT NULL,					-- the url from which to clone the repository 
+	runtime VARCHAR(1024) NOT NULL,						-- the runtime (binary, java, python, ...)
+	executable VARCHAR(1024) NOT NULL,					-- the path for the executable
+	arguments VARCHAR(1024) NOT NULL,					-- arguments for the execution (if needed (like the main java class name))
+	installed BOOLEAN NOT NULL DEFAULT FALSE,			-- if the plugin is currently installed
+	enabled BOOLEAN NOT NULL DEFAULT FALSE,				-- if the plugin is enabled aka if it can be used
+	UNIQUE(version, version_type, repository, runtime, executable, arguments)
 );
 
 CREATE TABLE plugin_relations (
-	id character varying(1024) NOT NULL PRIMARY KEY,
-	plugin_id character varying(1024) NOT NULL,
-	relation_id VARCHAR(1024) NOT NULL,
-	relation_type VARCHAR(1024) NOT NULL,
-	input_format VARCHAR(1024) NOT NULL,
-	output_format VARCHAR(1024) NOT NULL
+	id character varying(1024) NOT NULL PRIMARY KEY,					-- the id of the relation (generated when the relation is created)
+	plugin_id character varying(1024) NOT NULL REFERENCES plugin(id),	-- the id of the plugin (from the plugin table)
+	relation_id VARCHAR(1024) NOT NULL,									-- the instanceId of the distribution
+	input_format VARCHAR(1024) NOT NULL,								-- the file format expected by the plugin for the input
+	output_format VARCHAR(1024) NOT NULL,								-- the file format expected as the output from the plugin execution
+	UNIQUE(plugin_id, relation_id, input_format, output_format)
 );
-
 
 END;
